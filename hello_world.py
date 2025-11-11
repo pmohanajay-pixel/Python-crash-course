71f509884f627ba5a0956c1820bd412f48106a26
Ajay Mohan <your_github_email@example.com>
2025-11-11 16:06:28 -0600

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+print("Hello Python world!")
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+print(2 + 5)
+print(90 *8)
+print(16 / 2)
+print(3*5 + 10 - 8)
+print(3**3)
+print(0.1 + 0.2)
+
+dinosours_age = 65_000_000
+print(dinosours_age)
+universe_age = 13_800_000_000
+print(universe_age)
+
+
