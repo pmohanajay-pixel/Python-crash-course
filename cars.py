27e4a4856c2ed514386a777237d22b49d59bb421
p.mohanajay <p.mohanajay@gmail.com>
2025-11-11 18:23:56 -0600

    Add cars.py

---
+++ cars.py
@@ -0,0 +1,7 @@
+cars = ["Toyota", "Honda", "Ford", "BMW"]
+for car in cars:
+    print(f"{car} is a great car brand.")
+    print(f"I would love to own a {car} someday.\n")
+    print("happy driving!\n")
+    
+          
